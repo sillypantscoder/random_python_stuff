ad88b5caf1ad9ce6028c160ea76715adb4b154f8
Jason Roman <funjasonroman@gmail.com>
2022-05-02 16:03:31 -0600

    Added "Camera.py"

---
+++ camera.py
@@ -0,0 +1,28 @@
+import pygame
+import pygame.camera
+import subprocess
+
+pygame.camera.init()
+
+camlist = pygame.camera.list_cameras()
+if not camlist: exit()
+cam = pygame.camera.Camera(camlist[0],(640,480))
+cam.start()
+camimg = pygame.Surface((0, 0))
+
+screen = pygame.display.set_mode((640, 480))
+
+running = True
+c = pygame.time.Clock()
+while running:
+	for event in pygame.event.get():
+		if event.type == pygame.QUIT:
+			running = False
+		elif event.type == pygame.MOUSEBUTTONDOWN:
+			pygame.image.save(camimg, "camera_image.png")
+			subprocess.run(["python3", "weirdimage.py", "camera_image.png"])
+	if cam.query_image(): camimg = cam.get_image()
+	screen.fill((255, 255, 255))
+	screen.blit(camimg, (0, 0))
+	pygame.display.flip()
+	c.tick(60)
