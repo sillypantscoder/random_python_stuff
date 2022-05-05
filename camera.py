38fdf73ea957554f99e8744304782598bbb9a5e5
Jason Roman <funjasonroman@gmail.com>
2022-05-05 10:47:43 -0600

    New weirdness: Contrast, Mist, and Swirl!

--- camera.py
+++ camera.py
@@ -6,11 +6,12 @@ import subprocess
 import random
 from weirdimage import transforms
 import threading
+import ui
 
 pygame.camera.init()
 
 camlist = pygame.camera.list_cameras()
-if not camlist: exit()
+if not camlist: exit("No cameras detected")
 cam = pygame.camera.Camera(camlist[0],(640,480))
 cam.start()
 camimg = pygame.Surface((0, 0))
@@ -18,13 +19,28 @@ filterimg = pygame.Surface((0, 0))
 
 screensize = [640 * 2, 480]
 screen = pygame.display.set_mode(screensize, pygame.RESIZABLE)
+pygame.display.set_caption("Weird Image (Camera)")
+
+pygame.font.init()
+ui.init(screen, pygame.font.SysFont("monospace", 12))
+filter = ui.menu("Select Transform", [*[x.__name__ for x in transforms], "", "All"])
+if filter > len(transforms):
+	filter = False
+else:
+	filter = transforms[filter]
 
 def filterasync():
 	global filterimg
-	t = transforms.copy()
-	random.shuffle(t)
-	filterimg = camimg.copy()
-	for f in t: filterimg = f(filterimg)
+	if filter == False:
+		t = transforms.copy()
+		random.shuffle(t)
+		i = camimg.copy()
+		for f in t:
+			i = f(i)
+			filterimg = i.copy()
+	else:
+		i = camimg.copy()
+		filterimg = filter(i)
 
 running = True
 c = pygame.time.Clock()
@@ -34,18 +50,19 @@ while running:
 			running = False
 		elif event.type == pygame.MOUSEBUTTONDOWN:
 			pygame.image.save(filterimg, "camera_image.png")
-			subprocess.run(["python3", "imgopen.py", "camera_image.png"])
+			subprocess.Popen(["python3", "imgopen.py", "camera_image.png"])
 		elif event.type == pygame.VIDEORESIZE:
 			screensize = [*event.size]
 			screen = pygame.display.set_mode(screensize, pygame.RESIZABLE)
 	# Get images
 	c = pygame.time.Clock()
-	while cam.query_image(): camimg = cam.get_image()
+	while cam.query_image():
+		camimg = cam.get_image()
+	if threading.active_count() < 2: threading.Thread(target=filterasync, name="filter").start()
 	try:
 		screen.fill((255, 255, 255))
 		screen.blit(camimg, (0, 0))
 		screen.blit(filterimg, (screensize[0] - filterimg.get_width(), 0))
 	except: pass
-	if threading.active_count() < 2: threading.Thread(target=filterasync, name="filter").start()
 	pygame.display.flip()
 	c.tick(60)
--- weirdimage.py
+++ weirdimage.py
@@ -6,14 +6,14 @@ import subprocess
 import sys
 import math
 
-def chop(i: pygame.Surface):
+def chop(i: pygame.Surface) -> pygame.Surface:
 	r = i.get_rect()
 	return pygame.transform.chop(i, pygame.Rect(random.randint(0, r.right // 2), random.randint(0, r.bottom // 2), random.randint(0, r.right // 2), random.randint(0, r.bottom // 2)))
 
-def flip(i: pygame.Surface):
+def flip(i: pygame.Surface) -> pygame.Surface:
 	return pygame.transform.flip(i, True, True)
 
-def rotate(i: pygame.Surface):
+def rotate(i: pygame.Surface) -> pygame.Surface:
 	b = i.copy()
 	b.set_at((0, 0), (0, 0, 0))
 	r = pygame.transform.rotate(b, random.randint(0, 360))
@@ -22,7 +22,7 @@ def rotate(i: pygame.Surface):
 	b.blit(r, (0, 0))
 	return b
 
-def roll(i: pygame.Surface):
+def roll(i: pygame.Surface) -> pygame.Surface:
 	r = pygame.Surface(i.get_size())
 	offset = (random.randint(0, i.get_width() // 2), random.randint(0, i.get_height() // 2))
 	r.blit(i, offset)
@@ -31,34 +31,27 @@ def roll(i: pygame.Surface):
 	r.blit(i, (offset[0] - i.get_width(), offset[1] - i.get_height()))
 	return r.copy()
 
-def negate(i: pygame.Surface):
+def negate(i: pygame.Surface) -> pygame.Surface:
 	r = pygame.Surface(i.get_size())
 	r.fill((255, 255, 255))
 	r.blit(i, (0, 0), special_flags=pygame.BLEND_SUB)
 	return r
 
-def addborder(i: pygame.Surface):
+def addborder(i: pygame.Surface) -> pygame.Surface:
 	s = random.randint(2, 20)
 	r = pygame.Surface((i.get_width() + s + s, i.get_height() + s + s))
 	r.fill((random.randint(0, 255), random.randint(0, 255), random.randint(0, 255)))
 	r.blit(i, (s, s))
 	return r
 
-def addline(i: pygame.Surface):
+def addline(i: pygame.Surface) -> pygame.Surface:
 	r = i.copy()
 	frompos = (random.randint(0, r.get_width()), random.randint(0, r.get_height()))
 	topos = (random.randint(0, r.get_width()), random.randint(0, r.get_height()))
 	pygame.draw.line(r, (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255)), frompos, topos, random.randint(1, 20))
 	return r
 
-def addcopy(i: pygame.Surface):
-	r = i.copy()
-	cropped = pygame.Surface((random.randint(0, r.get_width() // 2), random.randint(0, r.get_height() // 2)))
-	cropped.blit(r, (random.randint(0, r.get_width() // 2), random.randint(0, r.get_height() // 2)))
-	r.blit(cropped, (random.randint(0, r.get_width() // 2), random.randint(0, r.get_height() // 2)))
-	return r
-
-def negate_random(i: pygame.Surface):
+def negate_random(i: pygame.Surface) -> pygame.Surface:
 	ind = [random.choice([False, False, True]), random.choice([False, False, True]), random.choice([False, False, True])]
 	r = i.copy()
 	for x in range(r.get_width()):
@@ -70,7 +63,7 @@ def negate_random(i: pygame.Surface):
 			r.set_at((x, y), this)
 	return r
 
-def shift(i: pygame.Surface):
+def shift(i: pygame.Surface) -> pygame.Surface:
 	r = i.copy()
 	for x in range(20, r.get_width() - 20):
 		for y in range(20, r.get_height() - 20):
@@ -84,11 +77,54 @@ def shift(i: pygame.Surface):
 			r.set_at(endpos, (this[0], this[1], this[2]))
 	return r
 
-def edges(i: pygame.Surface):
+def edges(i: pygame.Surface) -> pygame.Surface:
 	e = pygame.transform.laplacian(i)
 	return e
 
-transforms = [chop, flip, rotate, roll, negate, addborder, addline, addcopy, negate_random, shift, edges]
+def contrast(i: pygame.Surface) -> pygame.Surface:
+	r = i.copy()
+	for x in range(r.get_width()):
+		for y in range(r.get_height()):
+			this = r.get_at((x, y))
+			f = random.choice([lambda x: x // 2, lambda x: x * 2])
+			cf = lambda x: min(255, max(0, f(x)))
+			this = [cf(this[0]), cf(this[1]), cf(this[2])]
+			r.set_at((x, y), this)
+	return r
+
+def mist(i: pygame.Surface) -> pygame.Surface:
+	r = i.copy()
+	for x in range(r.get_width()):
+		for y in range(r.get_height()):
+			this = r.get_at((x, y))
+			shift = (random.randint(0, 255), random.randint(0, 255))
+			r.set_at((x + shift[0], y + shift[1]), this)
+	return r
+
+def swirl(i: pygame.Surface) -> pygame.Surface:
+	img = i.copy()
+	col0 = 0.5 * (float(img.get_width())  - 1.0)
+	row0 = 0.5 * (float(img.get_height()) - 1.0)
+	swirlBy = random.randint(2, 50)
+	swirlBy *= 100
+	# Compute the swirl.
+	for orig_col in range(img.get_width()):
+		for orig_row in range(img.get_height()):
+			# For each pixel:
+			rel_col = float(orig_col) - col0
+			rel_row = float(orig_row) - row0
+			dist = math.sqrt((rel_col * rel_col) + (rel_row * rel_row))
+			angle = (math.pi / float(swirlBy)) * dist
+			dest_col = int(rel_col * math.cos(angle) - rel_row * math.sin(angle) + col0)
+			dest_row = int(rel_col * math.sin(angle) + rel_row * math.cos(angle) + row0)
+			# Plot pixel (dest_x, dest_y) the same color as (orig_x, orig_y) if it's
+			# in bounds
+			if (dest_col >= 0) and (dest_col < img.get_width()) and \
+			(dest_row >= 0) and (dest_row < img.get_height()):
+				img.set_at((orig_col, orig_row), i.get_at((dest_col, dest_row)))
+	return img
+
+transforms = [chop, flip, rotate, roll, negate, addborder, addline, negate_random, shift, edges, contrast, mist, swirl]
 
 if __name__ == "__main__":
 	inputfilename = sys.argv[1]
@@ -101,5 +137,5 @@ if __name__ == "__main__":
 		# Progress bar
 		print(u"\u001b[1A\r\u001b[0K" + str(f + 1) + "/" + str(len(transforms)))
 	pygame.image.save(after, outputfilename)
-	#subprocess.Popen(["python3", "imageviewer.py", inputfilename])
-	#subprocess.run(["python3", "imageviewer.py", outputfilename])
+	subprocess.Popen(["python3", "imageviewer.py", inputfilename])
+	subprocess.run(["python3", "imageviewer.py", outputfilename])
