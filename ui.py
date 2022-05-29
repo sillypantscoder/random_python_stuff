d1e4946b5608d35634f7b92932b0d8aeed690c30
Jason Roman <funjasonroman@gmail.com>
2022-05-29 08:51:39 -0600

    === ADDED FOCUS MODEL!!! ===

--- ui.py
+++ ui.py
@@ -1,5 +1,6 @@
 import pygame
 import typing
+import helpers
 
 settings = {}
 
@@ -20,9 +21,16 @@ class UIElement:
 		r = pygame.Surface((1, 1))
 		r.fill(BLACK)
 		return r
-	def handleclick(self): pass
+	def handleclick(self, pos): pass
 	def addclick(self, func): pass
 	def __repr__(self): return "UIElement"
+
+class FocasableUIElement(UIElement):
+	"""Base class for an element in a UI that can be focused."""
+	def __init__(self):
+		self.focused = False
+	def handleclick(self, pos):
+		self.focused = True
 
 class Header(UIElement):
 	"""A header for a UI. Renders as a white text against a black background."""
@@ -62,7 +70,7 @@ class Option(UIElement):
 	def addclick(self, handler):
 		self.clickevents.append(handler)
 		return self
-	def handleclick(self):
+	def handleclick(self, pos):
 		for handler in self.clickevents:
 			handler()
 	def __repr__(self): return f"UIElement (Option \"{self.text}\")"
@@ -82,7 +90,7 @@ class Button(UIElement):
 	def addclick(self, handler):
 		self.clickevents.append(handler)
 		return self
-	def handleclick(self):
+	def handleclick(self, pos):
 		for handler in self.clickevents:
 			handler()
 	def __repr__(self): return f"UIElement (Button \"{self.text}\")"
@@ -105,6 +113,90 @@ class Image(UIElement):
 		return self.image
 	def __repr__(self): return f"UIElement (Image {self.image.get_width()}x{self.image.get_height()})"
 
+class KeyboardInput(FocasableUIElement):
+	"""A UI element that displays a text box and allows the user to enter text."""
+	def __init__(self, text: str, allowedChars: str = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789_"):
+		super().__init__()
+		self.text = text
+		self.allowedChars = allowedChars
+	def render(self, mouse, width):
+		# Render the text box
+		retext = settings["font"].render(self.text + ("|" if self.focused else ""), True, BLACK)
+		r = pygame.Surface((width - 20, retext.get_height()))
+		r.fill(WHITE)
+		r.blit(retext, (0, 0))
+		# Render the border
+		sol = pygame.Surface((width, r.get_height() + 20))
+		sol.fill(BLACK)
+		if self.focused:
+			pygame.draw.rect(sol, (255, 255, 255), (5, 5, width - 10, r.get_height() + 10), 1)
+		sol.blit(r, (10, 10))
+		return sol
+	def __repr__(self): return f"UIElement (KeyboardInput \"{self.text}\")"
+
+class Dropdown(FocasableUIElement):
+	"""A UI element that displays a dropdown menu."""
+	def __init__(self, options: "list[str]"):
+		super().__init__()
+		self.options = options
+		self.selected = 0
+	def render_all_options(self, width) -> "list[pygame.Surface]":
+		optionlist = []
+		for item in range(len(self.options)):
+			# Render the text box
+			retext = settings["font"].render(self.options[item], True, BLACK)
+			r = pygame.Surface((width - 20, retext.get_height()))
+			r.fill(WHITE)
+			r.blit(retext, (0, 0))
+			# Render the border
+			sol = pygame.Surface((width, r.get_height() + 20))
+			sol.fill(WHITE)
+			pygame.draw.rect(sol, BLACK, pygame.Rect(0, 0, 10, sol.get_height()))
+			sol.blit(r, (10, 10))
+			optionlist.append(sol)
+		return optionlist
+	def render(self, mouse, width):
+		# Render the text box
+		retext = settings["font"].render(self.options[self.selected], True, WHITE)
+		r = pygame.Surface((width - 20, retext.get_height()))
+		r.fill(BLACK)
+		r.blit(retext, (0, 0))
+		# Render the border
+		sol = pygame.Surface((width, r.get_height() + 20))
+		sol.fill(BLACK)
+		padding = 10
+		arrow = [(padding, padding), (sol.get_height() - (padding * 2), sol.get_height() // 2), (padding, sol.get_height() - padding)]
+		if self.focused:
+			pygame.draw.rect(sol, WHITE, (5, 5, width - 10, r.get_height() + 10), 1)
+			pygame.draw.polygon(sol, WHITE, [helpers.rotateC(((sol.get_height() // 4) + (padding // 2), sol.get_height() // 2), p, 90) for p in arrow])
+		else:
+			# Draw triangle
+			pygame.draw.polygon(sol, WHITE, arrow)
+		sol.blit(r, (10 + (padding * 1.5), 10))
+		# sol is the header
+		# Render the options
+		if self.focused:
+			optionlist = self.render_all_options(width)
+			for item in range(len(optionlist)):
+				# Extend the surface
+				newItem = pygame.Surface((width, sol.get_height() + optionlist[item].get_height()))
+				newItem.fill(WHITE)
+				newItem.blit(sol, (0, 0))
+				newItem.blit(optionlist[item], (0, sol.get_height()))
+				sol = newItem.copy()
+		return sol
+	def handleclick(self, pos):
+		super().handleclick(pos)
+		# Get the position of the click
+		optionlist = self.render_all_options(500)
+		y = pos[1] // optionlist[0].get_height()
+		if y == 0:
+			# clicked on header
+			return
+		self.selected = y - 1
+		self.focused = False
+	def __repr__(self): return f"UIElement (Dropdown \"{self.options[self.selected]}\")"
+
 class UI:
 	"""A UI to be drawn to the screen. Contains a list of UIElements."""
 	def __init__(self):
@@ -116,20 +208,26 @@ class UI:
 		for item in items:
 			self.add(item)
 		return self
-	def render(self, mousepos, mouseclicked):
+	def render(self, mousepos: "tuple[int, int]", mouseclicked: bool) -> pygame.Surface:
 		scrn_width, scrn_height = settings["screen"].get_size()
+		# Render the items:
 		rendered_items = []
 		cum_y = 0
 		for item in self.items:
 			i = item.render(False, scrn_width)
 			if i.get_rect().move(0, cum_y).collidepoint(mousepos):
 				i = item.render(True, scrn_width)
-				if mouseclicked:
-					item.handleclick()
+			if mouseclicked:
+				if i.get_rect().move(0, cum_y).collidepoint(mousepos):
+					item.handleclick((mousepos[0], mousepos[1] - cum_y))
+				else:
+					if isinstance(item, FocasableUIElement):
+						item.focused = False
 			rendered_items.append(i)
 			if i.get_width() > scrn_width:
 				scrn_width = i.get_width()
 			cum_y += i.get_height()
+		# Draw all the items onto the screen
 		ret = pygame.Surface((scrn_width, scrn_height))
 		ret.fill(WHITE)
 		cum_y = 0
@@ -140,7 +238,7 @@ class UI:
 	def __repr__(self): return f"UI [ {', '.join([str(i) for i in self.items])} ]"
 
 def render_ui(ui: UI):
-	"""Renders a UI to the screen."""
+	"""Renders a UI to the screen, with keyboard input."""
 	clicked = False
 	for event in pygame.event.get():
 		if event.type == pygame.QUIT:
@@ -150,6 +248,15 @@ def render_ui(ui: UI):
 			clicked = True
 		if event.type == pygame.VIDEORESIZE:
 			settings["screen"] = pygame.display.set_mode(event.dict['size'], pygame.RESIZABLE)
+		if event.type == pygame.KEYDOWN:
+			if event.key == pygame.K_BACKSPACE:
+				for item in ui.items:
+					if isinstance(item, KeyboardInput) and item.focused:
+						item.text = item.text[:-1]
+			for item in ui.items:
+				if isinstance(item, KeyboardInput) and item.focused:
+					if event.unicode in item.allowedChars:
+						item.text += event.unicode
 	m = pygame.mouse.get_pos()
 	settings["screen"].blit(ui.render(m, clicked), (0, 0))
 	pygame.display.flip()
